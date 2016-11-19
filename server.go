c3fb6cca8ecf3d26a604ee90c82e3f81b0d31c5a
Killian Davitt <davittk@tcd.ie>
2016-11-19 13:11:43 +0000

    Added buffered readers, has improved stability, tweaks all round

--- server.go
+++ server.go
@@ -6,8 +6,13 @@ import (
 	"os"
 	"strings"
 	"log"
+	"bufio"
 )
-
+type user struct {
+	reader *bufio.Reader
+	writer *bufio.Writer
+	username string	
+}
 
 func main(){
 	port := os.Args[1]
@@ -16,8 +21,7 @@ func main(){
 	if err != nil {
 	    fmt.Println("Fatal Error")
 	}
-	rooms := make(map[string]chan net.Conn)
-
+	rooms := make(map[string]chan user)
 	
 	terminate_chan := make(chan bool)
 	for {
@@ -28,57 +32,68 @@ func main(){
 		}
 		go handleConnection(conn, &listener, terminate_chan, rooms)
 	}
-
 }
 
-func handleConnection(conn net.Conn, listener *net.Listener, terminate_chan chan bool, rooms map[string]chan net.Conn){
-	buf := make([]byte, 1024)
-	n, _ := conn.Read(buf)
-	interpretMessage(buf[:n], conn, terminate_chan, rooms)
-	
+func handleConnection(conn net.Conn, listener *net.Listener, terminate_chan chan bool, rooms map[string]chan user){
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+	l1, _ := reader.ReadString(byte('\n'))
+	l2,_ := reader.ReadString(byte('\n'))
+	l3,_ := reader.ReadString(byte('\n'))
+	l4,_ := reader.ReadString(byte('\n'))
+
+	message := l1+l2+l3+l4
+	//n, _ := conn.Read(buf)
+	interpretMessage(message, reader, writer, terminate_chan, rooms)
 }
 
-func interpretMessage(byte_message []byte, conn net.Conn, terminate_chan chan bool, rooms map[string]chan net.Conn){
+func interpretMessage(message string, reader *bufio.Reader, writer *bufio.Writer, terminate_chan chan bool, rooms map[string]chan user){
 	// Dict of rooms with channels, send new connections via the socket to the thread.
-	message := string(byte_message)
 	log.Print("Interpreting message " + message)
 	attribs := strings.Split(message, "\n")
+	log.Print(len(attribs))
+	if len(attribs) < 3 {
+		return
+	}
 	log.Print(attribs[0][0:13])
 	if attribs[0][0:13] == "JOIN_CHATROOM" {
 		log.Print("User is joining a room")
+		username := strings.Join(strings.Split(attribs[3], ":")[1:], " ")
+		var new_user_obj user
+		new_user := &new_user_obj
+		new_user.username = username
+		new_user.reader = reader
+		new_user.writer = writer
 		roomName := attribs[0][13:]
 		room := rooms[roomName]
 		if room == nil {
-			rooms[roomName] = make(chan net.Conn)
+			rooms[roomName] = make(chan user)
 			log.Print("Creating chat room")
-			go chatRoom(conn, rooms[roomName])
-			conn.Write([]byte("chatroom has been created"))
+			go chatRoom(new_user, rooms[roomName], roomName)
+			writer.Write([]byte("chatroom has been created\n"))
 		} else {
 			// Room already exists, send the conn in  the channel
-			room <- conn
-			conn.Write([]byte("You have been connected"))
+			room <- *new_user
+			writer.Write([]byte("You have been connected\n"))
 		}
 	}
 	
 }
 
-func chatRoom(initial_user net.Conn, room_channel chan net.Conn){
-	users := make([]net.Conn, 0, 0)
-	users = append(users, initial_user)
+func chatRoom(initial_user *user, room_channel chan user, roomName string){
+	users := make([]user, 0, 0)
+	users = append(users, *initial_user)
 	for {
-		newUser := <- room_channel
-		if newUser != nil {
+		select {
+			case newUser := <- room_channel:
 			users = append(users, newUser)
-		}
-
-		for i:=0; i<len(users); i++ {
-			buf := make([]byte, 1024)
-			n, err := users[i].Read(buf)
-			log.Print("User sent message: " + string(buf))
-			if err != nil {
-				fmt.Println(err)
+		default:
+			for i:=0; i<len(users); i++ {
+				mesg, _ := users[i].reader.ReadString('\n')
+				log.Print("User sent message: " + mesg)
+				sendMessages(mesg, users, users[i], roomName)
 			}
-			sendMessages(buf[:n], users)
+
 		}
 	}
 }
@@ -86,10 +101,12 @@ func chatRoom(initial_user net.Conn, room_channel chan net.Conn){
 /*
    sendMessages takes a message, a sender and a list of the users connections whom are in the chatroom
 */
-func sendMessages(message []byte, users []net.Conn){
+func sendMessages(message string, users []user, sender user, roomName string){
 	for i:=0; i<len(users); i++ {
-		users[i].Write(message)
+		log.Print("Sending...")
+		if users[i] != sender {
+			mesg := "CHAT: " + roomName + "\nCLIENT_NAME: " + sender.username + "\nMESSAGE:" + string(message) + "\n\n"
+			users[i].writer.Write([]byte(mesg))
+		}
 	}
 }
-
-
