99e6786ce9861cd56deb1eb220e768a601aaefa1
Killian Davitt <davittk@tcd.ie>
2016-12-02 13:25:01 +0000

    forgot to add killservice functionality

--- server.go
+++ server.go
@@ -1,53 +1,58 @@
-package main;
+package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"strings"
-	"log"
-	"bufio"
 )
+
 type user struct {
-	reader *bufio.Reader
-	writer *bufio.Writer
-	username string	
+	reader   *bufio.Reader
+	writer   *bufio.Writer
+	username string
 }
 
-func main(){
+func main() {
 	port := os.Args[1]
 	port = ":" + port
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
-	    fmt.Println("Fatal Error")
+		fmt.Println("Fatal Error")
 	}
 	rooms := make(map[string]chan user)
-	
+
 	terminate_chan := make(chan bool)
 	for {
 		conn, err := listener.Accept()
 		defer conn.Close()
 		if err != nil {
-			    fmt.Println("Fatal Error")
+			fmt.Println("Fatal Error")
 		}
 		go handleConnection(conn, &listener, terminate_chan, rooms)
 	}
 }
 
-func handleConnection(conn net.Conn, listener *net.Listener, terminate_chan chan bool, rooms map[string]chan user){
+func handleConnection(conn net.Conn, listener *net.Listener, terminate_chan chan bool, rooms map[string]chan user) {
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
 	l1, _ := reader.ReadString(byte('\n'))
-	l2,_ := reader.ReadString(byte('\n'))
-	l3,_ := reader.ReadString(byte('\n'))
-	l4,_ := reader.ReadString(byte('\n'))
 
-	message := l1+l2+l3+l4
+	if l1 == "KILL_SERVICE" {
+		os.Exit(0)
+	}
+	l2, _ := reader.ReadString(byte('\n'))
+	l3, _ := reader.ReadString(byte('\n'))
+	l4, _ := reader.ReadString(byte('\n'))
+
+	message := l1 + l2 + l3 + l4
 	//n, _ := conn.Read(buf)
 	interpretMessage(message, reader, writer, terminate_chan, rooms)
 }
 
-func interpretMessage(message string, reader *bufio.Reader, writer *bufio.Writer, terminate_chan chan bool, rooms map[string]chan user){
+func interpretMessage(message string, reader *bufio.Reader, writer *bufio.Writer, terminate_chan chan bool, rooms map[string]chan user) {
 	// Dict of rooms with channels, send new connections via the socket to the thread.
 	log.Print("Interpreting message " + message)
 	attribs := strings.Split(message, "\n")
@@ -77,18 +82,18 @@ func interpretMessage(message string, reader *bufio.Reader, writer *bufio.Writer
 			writer.Write([]byte("You have been connected\n"))
 		}
 	}
-	
+
 }
 
-func chatRoom(initial_user *user, room_channel chan user, roomName string){
+func chatRoom(initial_user *user, room_channel chan user, roomName string) {
 	users := make([]user, 0, 0)
 	users = append(users, *initial_user)
 	for {
 		select {
-			case newUser := <- room_channel:
+		case newUser := <-room_channel:
 			users = append(users, newUser)
 		default:
-			for i:=0; i<len(users); i++ {
+			for i := 0; i < len(users); i++ {
 				mesg, _ := users[i].reader.ReadString('\n')
 				log.Print("User sent message: " + mesg)
 				sendMessages(mesg, users, users[i], roomName)
@@ -101,8 +106,8 @@ func chatRoom(initial_user *user, room_channel chan user, roomName string){
 /*
    sendMessages takes a message, a sender and a list of the users connections whom are in the chatroom
 */
-func sendMessages(message string, users []user, sender user, roomName string){
-	for i:=0; i<len(users); i++ {
+func sendMessages(message string, users []user, sender user, roomName string) {
+	for i := 0; i < len(users); i++ {
 		log.Print("Sending...")
 		if users[i] != sender {
 			mesg := "CHAT: " + roomName + "\nCLIENT_NAME: " + sender.username + "\nMESSAGE:" + string(message) + "\n\n"
