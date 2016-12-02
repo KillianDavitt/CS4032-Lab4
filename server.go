e6d5b9f7982063a1fd5b340025c81f0105cfdb85
Killian Davitt <davittk@tcd.ie>
2016-12-02 19:36:33 +0000

    testing

--- server.go
+++ server.go
@@ -124,7 +124,7 @@ func sendMessages(message string, users []user, sender user, roomName string) {
 	for i := 0; i < len(users); i++ {
 		log.Print("Sending...")
 		if users[i] != sender {
-
+			log.Print(sender.username)
 			mesg := "CHAT: " + roomName + "\nCLIENT_NAME:" + sender.username + "\nMESSAGE:" + string(message) + "\n\n"
 			users[i].writer.Write([]byte(mesg))
 			users[i].writer.Flush()
