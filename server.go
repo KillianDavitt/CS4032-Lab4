f813aaca2f00bf4a5ce36e304a1bcf672f292f3e
Killian Davitt <davittk@tcd.ie>
2016-12-02 18:37:37 +0000

    Further testing

--- server.go
+++ server.go
@@ -84,7 +84,7 @@ func interpretMessage(message string, reader *bufio.Reader, writer *bufio.Writer
 			rooms[roomName] = make(chan user)
 			log.Print("Creating chat room")
 			go chatRoom(new_user, rooms[roomName], roomName)
-			reply := "JOINED_CHATROOM:" + roomName + "\nSERVER_IP: 10.62.0.83\nPORT: 8000\nROOM_REF: [integer that uniquely identifies chat room on server]\nJOIN_ID: [integer that uniquely identifies client joining]"
+			reply := "JOINED_CHATROOM:" + roomName + "\nSERVER_IP: 10.62.0.83\nPORT: 8000\nROOM_REF: 1\nJOIN_ID: 2"
 			writer.Write([]byte(reply))
 			writer.Flush()
 		} else {
